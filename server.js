6a21504447ccbc5594e4b4b829211bbaf3690a3e
Anjana Induwara <36652213+Devanjana360@users.noreply.github.com>
2023-10-23 06:22:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const connectMongodb = async () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://fiverr-web.netlify.app"],
+    origin: ["http://localhost:5173", "https://front-swart-psi.vercel.app"],
     credentials: true,
   })
 );
